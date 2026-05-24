6853439d76d511dd88424d251c73dffc9a845339
Night <mateuzinho4866@gmail.com>
2026-05-24 15:47:34 -0300

    Update Loader.lua for game support and error handling

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,7 @@
 --// Zenith Hub - Loader
 
 if getgenv().ZenithHubLoaded then
+    warn("Zenith Hub already loaded.")
     return
 end
 
@@ -8,25 +9,32 @@ getgenv().ZenithHubLoaded = true
 
 repeat task.wait() until game:IsLoaded()
 
+--====================================================
+-- SUPPORTED GAMES
+--====================================================
+
 local Supported = {
-    [2753915549] = true,
-    [4442272183] = true,
-    [7449423635] = true
+    [2753915549] = true, -- Sea 1
+    [4442272183] = true, -- Sea 2
+    [7449423635] = true  -- Sea 3
 }
 
 if not Supported[game.PlaceId] then
     return warn("Game not supported.")
 end
 
-local URL =
-"https://raw.githubusercontent.com/Pixelpv/Zenith-Hub/refs/heads/main/Main.lua"
+--====================================================
+-- LOAD MAIN
+--====================================================
 
 local Success, Error = pcall(function()
 
-    loadstring(game:HttpGet(URL))()
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Pixelpv/Zenith-Hub/main/Main.lua"
+    ))()
 
 end)
 
 if not Success then
-    warn(Error)
+    warn("Loader Error:", Error)
 end
